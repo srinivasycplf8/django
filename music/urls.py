b843ac9551f0e363df78d7a22b853d8f5dc028ad
Naga Sai Srinivas <srinivas.ycplf8@gmail.com>
2020-07-26 16:05:22 +0530

    Views

---
+++ music/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    #the '' is used when the user didin't select specific song or default it
+    #goes to views page and their function mae called index 
+    #so whathtver it does it returns our value
+    path('', views.index,name="index")
+]
