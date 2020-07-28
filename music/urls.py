7161b5d3a7feac9b20e69ba7dab43f5847623a04
Naga Sai Srinivas <srinivas.ycplf8@gmail.com>
2020-07-28 07:53:12 +0530

    Writing another View

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,20 @@
 from . import views
-from django.urls import path
+from django.urls import path,re_path
 
 urlpatterns = [
     #the '' is used when the user didin't select specific song or default it
     #goes to views page and their function mae called index 
     #so whathtver it does it returns our value
-    path('', views.index,name="index")
+
+    ##for using regular expression we have to use re_path
+    ##instead of ""path"""
+    re_path(r'^$', views.index,name="index"),
+
+    ##whenever I click the album,it gives mae all songs
+    #so we have to pass an id 
+
+    #/music/712/
+
+    re_path(r'^(?P<album_id>[0-9]+)/$',views.detail,name="detail"),
     
 ]
--- music/views.py
+++ music/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the Music APP</h1>")
+
+def detail(request,album_id):
+    return HttpResponse("<h2>Details of the songs are"+str(album_id)+"</h2>")
