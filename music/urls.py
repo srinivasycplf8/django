00ca89736c0a2e118b6668a4c9d241331bb9703f
Naga Sai Srinivas <srinivas.ycplf8@gmail.com>
2020-07-30 10:57:34 +0530

    Update and Delete View

--- music/urls.py
+++ music/urls.py
@@ -1,15 +1,11 @@
+from music.views import AlbumDelete
 from . import views
 from django.urls import path,re_path
 
 app_name='music'
 
 urlpatterns = [
-    #the '' is used when the user didin't select specific song or default it
-    #goes to views page and their function mae called index 
-    #so whathtver it does it returns our value
-
-    ##for using regular expression we have to use re_path
-    ##instead of ""path"""
+  
     re_path(r'^$', views.IndexView.as_view(),name="index"),
 
     ##whenever I click the album,it gives mae all songs
@@ -22,6 +18,14 @@ urlpatterns = [
     #music/album/add/
 
     re_path(r'album/add/$',views.AlbumCreate.as_view(),name="album-add"),
+
+    #music/album/2/
+
+    re_path(r'^album/(?P<pk>[0-9]+)/$',views.AlbumUpdate.as_view(),name="album-update"),
+
+    #music/album/2/delete
+
+    re_path(r'album/(?P<pk>[0-9]+)/delete/$',views.AlbumDelete.as_view(),name="album-delete"),
 
 
 
